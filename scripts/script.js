cef90fef36c78be8678cf7187d41ac2fc8dfbfcd
Riley Loudon <rileyloudon@icloud.com>
2020-02-07 22:38:16 -0600

    allow user to set work time, add start button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,60 @@
 const mainTimer = document.getElementById('main-timer');
 
-// 25 minutes from now.
-let time = new Date().getTime() + 1500000;
+const setTime = {
+  workTime: null,
+  // workTime: 5000,
+  breakTime: 11000,
+};
 
-const countdown = () => {};
+const userInput = document.getElementById('work-time');
+userInput.addEventListener('input', () => {
+  if (isNaN(userInput.value)) {
+    mainTimer.innerHTML = 'Please enter a number.';
+  } else {
+    mainTimer.innerHTML = userInput.value + ':00s';
+  }
+});
 
-setInterval(() => {
-  let now = new Date().getTime();
+const setDisplay = () => {
+  mainTimer.innerHTML = userInput.value + ':00s';
+};
 
-  let timeLeft = time - now;
-  let minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
-  let seconds = Math.floor((timeLeft / 1000) % 60);
+const workCountdown = () => {
+  let time = new Date().getTime() + setTime.workTime;
+  let next = 'break';
+  runCountdown(time, next);
+};
 
-  mainTimer.innerHTML = minutes + ':' + seconds + 's';
-}, 1000);
+const breakCountdown = () => {
+  let time = new Date().getTime() + setTime.breakTime;
+  let next = 'work';
+  runCountdown(time, next);
+};
+
+const runCountdown = (time, next) => {
+  let countdown = setInterval(() => {
+    let now = new Date().getTime();
+
+    let timeLeft = time - now;
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    if (timeLeft < 100) {
+      clearInterval(countdown);
+      next === 'work' ? workCountdown() : breakCountdown();
+    }
+    mainTimer.innerHTML = minutes + ':' + seconds + 's';
+  }, 100);
+};
+
+const play = document.getElementById('play');
+play.addEventListener('click', () => {
+  if (!isNaN(userInput.value)) {
+    setTime.workTime = userInput.value * 1000 * 60 + 1000;
+    workCountdown();
+  }
+});
+
+setDisplay();
