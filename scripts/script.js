21c573651f4f20fdd824c2e4f2936e3bbd449172
Riley Loudon <rileyloudon@icloud.com>
2020-02-09 18:24:50 -0600

    check and update user inputted times

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,45 @@
 const mainTimer = document.getElementById('main-timer');
 
+// Defaults times: 25 minute work, 5 minute break.
 const setTime = {
-  workTime: null,
-  // workTime: 5000,
-  breakTime: 11000,
+  workTime: 1501000,
+  breakTime: 6000,
 };
 
-const userInput = document.getElementById('work-time');
-
-userInput.addEventListener('input', () => {
-  if (isNaN(userInput.value)) {
-    mainTimer.innerHTML = 'Please enter a number.';
-  } else {
-    mainTimer.innerHTML = userInput.value + ':00s';
-  }
+const userInputWork = document.getElementById('work-time');
+userInputWork.addEventListener('input', () => {
+  checkInput(userInputWork.value);
 });
 
-const setDisplay = () => {
-  mainTimer.innerHTML = userInput.value + ':00s';
+const userInputBreak = document.getElementById('break-time');
+userInputBreak.addEventListener('input', () => {
+  checkInput(userInputBreak.value);
+});
+
+const checkInput = input => {
+  isNaN(input) || input < 0 || input > 59
+    ? (mainTimer.innerHTML = 'Enter 0-59')
+    : !userInputWork.value
+    ? (mainTimer.innerHTML = '25:00s')
+    : (mainTimer.innerHTML = userInputWork.value + ':00s');
 };
+
+const setDisplay = () => {
+  !userInputWork.value
+    ? (mainTimer.innerHTML = '25:00s')
+    : (mainTimer.innerHTML = userInputWork.value + ':00s');
+};
+
+const play = document.getElementById('play');
+play.addEventListener('click', () => {
+  console.log(userInputWork);
+  if (!isNaN(userInputWork.value)) {
+    if (userInputWork.value) {
+      setTime.workTime = userInputWork.value * 1000 * 60 + 1000;
+    }
+    workCountdown();
+  }
+});
 
 const workCountdown = () => {
   let time = new Date().getTime() + setTime.workTime;
@@ -35,7 +56,6 @@ const breakCountdown = () => {
 const runCountdown = (time, next) => {
   let countdown = setInterval(() => {
     let now = new Date().getTime();
-
     let timeLeft = time - now;
     let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
@@ -49,13 +69,5 @@ const runCountdown = (time, next) => {
     mainTimer.innerHTML = minutes + ':' + seconds + 's';
   }, 100);
 };
-
-const play = document.getElementById('play');
-play.addEventListener('click', () => {
-  if (!isNaN(userInput.value)) {
-    setTime.workTime = userInput.value * 1000 * 60 + 1000;
-    workCountdown();
-  }
-});
 
 setDisplay();
